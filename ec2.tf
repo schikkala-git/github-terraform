9919f11bea921b7d53b3e7aa41f77b5c9a667861
srini@gmail.com <bobbychikkala@Bobbys-iMac.local>
2025-07-25 10:46:17 -0400

    added .pem file

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "ec2_sg" {
   name        = "ec2-sg-ssh"
   description = "Allow SSH from anywhere"
-  vpc_id      = "vpc-0188603251f260c53"  # <-- Replace with your VPC ID
+  vpc_id      = "vpc-0188603251f260c53"
 
   ingress {
     description = "SSH access"
@@ -20,19 +20,19 @@ resource "aws_security_group" "ec2_sg" {
   }
 
   tags = {
-    Name = "ec2-sg-ssh" 
-    Environment="prod-instance"
+    Name        = "ec2-sg-ssh"
+    Environment = "prod-instance"
   }
 }
 
 resource "aws_instance" "private_ec2" {
-  ami                    = "ami-020cba7c55df1f615"  # Amazon Linux 2 (update as needed)
+  ami                    = "ami-020cba7c55df1f615"  # Amazon Linux 2 (us-east-1)
   instance_type          = "t2.micro"
-  subnet_id              = "subnet-075332923667ce70a"        # <-- Replace with your Private Subnet ID
+  subnet_id              = "subnet-075332923667ce70a"
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
-  key_name               = "my-keypair"             # <-- Optional: Replace with your key pair name
+  key_name               = "srini"  # Your key pair name
 
-  associate_public_ip_address = false  # Since it's in a private subnet
+  associate_public_ip_address = false
 
   tags = {
     Name = "PrivateEC2Instance"
